6bf79fceb3a17c36a1de3767efdce241e0e4b0b2
nuttycc <73019368+nuttycc@users.noreply.github.com>
2025-03-14 13:53:36 +0800

    refactor(bun_server): format server.js

--- bun_server/server.js
+++ bun_server/server.js
@@ -1,89 +1,97 @@
-import cookie from 'cookie'
+import cookie from "cookie";
 import { CLIENTS } from "./src/utils/clients";
-import { faker } from '@faker-js/faker';
-import { isValidJson } from './src/utils/jsonx';
-
-
+import { faker } from "@faker-js/faker";
+import { isValidJson } from "./src/utils/jsonx";
 
 const server = Bun.serve({
-  fetch(req, server) {
-    const cookies = cookie.parse(req.headers.get('cookie') || '')
-    const id =  faker.lorem.word()
+	fetch(req, server) {
+		const cookies = cookie.parse(req.headers.get("cookie") || "");
+		const id = faker.lorem.word();
 
-    // upgrade the request to a WebSocket
-    const success = server.upgrade(req, {
-      headers: {
-        'Set-Cookie': `SessionId=${id}`
-      },
-      data: {
-        id: id,
-        username: faker.internet.username(),
-        createAt: Date.now(),
-      },
-    })
-    if(!success) {
-      console.log ('Failed to upgrade to WebSocket')
-    }
-    return success ? undefined : new Response('Failed to upgrade to WebSocket', { status: 400 })
-  },
-  websocket: {
-    message(ws, message) {
-      // server.publish('test-chat', `Echo from server: ${message}`)
-      
-      if(isValidJson(message)) {
-        const msg = JSON.parse(message)
-        if(msg.type === 'ice' || msg.type === 'offer' || msg.type === 'answer') {
-          const targetWs = CLIENTS.get(msg.to);
-          if (targetWs) {
-            targetWs.send(message); // 确保存在再发送
-          } else {
-            console.error(`Target client ${msg.to} not found`);
-          }
-        }
-      }
+		// upgrade the request to a WebSocket
+		const success = server.upgrade(req, {
+			headers: {
+				"Set-Cookie": `SessionId=${id}`,
+			},
+			data: {
+				id: id,
+				username: faker.internet.username(),
+				createAt: Date.now(),
+			},
+		});
+		if (!success) {
+			console.log("Failed to upgrade to WebSocket");
+		}
+		return success
+			? undefined
+			: new Response("Failed to upgrade to WebSocket", { status: 400 });
+	},
+	websocket: {
+		message(ws, message) {
+			// server.publish('test-chat', `Echo from server: ${message}`)
 
-    }, 
-    open(ws) {
-      ws.send('Server Opened!');
+			if (isValidJson(message)) {
+				const msg = JSON.parse(message);
+				if (
+					msg.type === "ice" ||
+					msg.type === "offer" ||
+					msg.type === "answer"
+				) {
+					const targetWs = CLIENTS.get(msg.to);
+					if (targetWs) {
+						targetWs.send(message); // 确保存在再发送
+					} else {
+						console.error(`Target client ${msg.to} not found`);
+					}
+				}
+			}
+		},
+		open(ws) {
+			ws.send("Server Opened!");
 
-      CLIENTS.set(ws.data.id, ws)
+			CLIENTS.set(ws.data.id, ws);
 
-      ws.send(JSON.stringify({
-        type: 'welcome',
-        from: 'system',
-        to: 'new-user',
-        data: ws.data,
-        clients: Array.from(CLIENTS.keys()), //已经存在的 clients
-      }))
-    
-      ws.subscribe('test-chat')
-      server.publish('test-chat', JSON.stringify({
-        type: 'welcome',
-        from: 'system',
-        to: 'all',
-        data: ws.data,
-      }))
-    },
-    close(ws, code, message) {
-      ws.send('Server Closed!');
+			ws.send(
+				JSON.stringify({
+					type: "welcome",
+					from: "system",
+					to: "new-user",
+					data: ws.data,
+					clients: Array.from(CLIENTS.keys()), //已经存在的 clients
+				}),
+			);
 
-      ws.unsubscribe('test-chat')
-      server.publish('test-chat', JSON.stringify({
-        type:'leave',
-        from: 'system',
-        to: 'all',
-        data: ws.data,
-      }))
+			ws.subscribe("test-chat");
+			server.publish(
+				"test-chat",
+				JSON.stringify({
+					type: "welcome",
+					from: "system",
+					to: "all",
+					data: ws.data,
+				}),
+			);
+		},
+		close(ws, code, message) {
+			ws.send("Server Closed!");
 
-      CLIENTS.delete(ws.data.id)
-    }, 
-    drain(ws) {
-      ws.send('Server Drain!');
-    }, 
-  },
+			ws.unsubscribe("test-chat");
+			server.publish(
+				"test-chat",
+				JSON.stringify({
+					type: "leave",
+					from: "system",
+					to: "all",
+					data: ws.data,
+				}),
+			);
+
+			CLIENTS.delete(ws.data.id);
+		},
+		drain(ws) {
+			ws.send("Server Drain!");
+		},
+	},
 });
 
-
 console.log(`Listening on ${server.hostname}:${server.port}...`);
-
-
